2896f46ca40899c87b9f829db18cca86a6b4e1ed
ricardoggz <ricardo_ggzicm@outlook.com>
2023-01-05 10:23:17 -0600

    Cors instalado para el uso de la api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import express from "express"
+import cors from 'cors'
 import { UserController, getCourses } from './controllers/index.js'
 
 const app = express()
 app.get('/', UserController)
 app.get('/courses', getCourses)
 app.use(express.json())
+app.use(cors())
 app.listen(3030)
