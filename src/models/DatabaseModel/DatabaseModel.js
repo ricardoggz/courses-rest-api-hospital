40932decbc337354d2886123310f14c6b745872d
ricardoggz <ricardo_ggzicm@outlook.com>
2023-01-24 09:08:02 -0600

    Variables de entorno creadas

--- src/models/DatabaseModel/DatabaseModel.js
+++ src/models/DatabaseModel/DatabaseModel.js
@@ -1,17 +1,22 @@
 import mysql from 'mysql'
+import * as dotenv from 'dotenv' 
+
+dotenv.config()
 
 export class DatabaseModel{
-    host = 'localhost'
-    user = 'root'
-    database = 'cursos-plataforma'
-    password = ''
+    host = process.env.HOST
+    user = process.env.USER
+    database = process.env.DATABASE
+    password = process.env.PASSWORD
+    port = process.env.PORT
     pool
     constructor(){
         this.pool = new mysql.createPool({
             host: this.host,
             user: this.user,
             database : this.database,
-            password: this.password
+            password: this.password,
+            port: this.port
         })
     }
 }
