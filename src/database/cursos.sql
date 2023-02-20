b34fcb9c19acaa363913a96ebe38ebdc0629f46e
ricardoggz <ricardo_ggzicm@outlook.com>
2023-02-19 21:25:24 -0600

    [change]: Database changed

--- src/database/cursos.sql
+++ src/database/cursos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-02-2023 a las 20:20:54
+-- Tiempo de generación: 20-02-2023 a las 02:51:28
 -- Versión del servidor: 10.4.20-MariaDB
 -- Versión de PHP: 8.0.8
 
@@ -37,46 +37,69 @@ CREATE TABLE `cursos` (
   `modality_id` int(11) DEFAULT NULL,
   `course_start_date` date NOT NULL,
   `course_finish_date` date NOT NULL,
-  `month_id` int(11) NOT NULL
+  `month_id` int(11) NOT NULL,
+  `course_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `cursos`
 --
 
-INSERT INTO `cursos` (`course_name`, `course_instructor`, `course_price`, `course_place`, `course_image`, `course_pdf`, `modality_id`, `course_start_date`, `course_finish_date`, `month_id`) VALUES
-('5to Congreso de enfermería en alta especialidad de la CCINSHAE', 'E.E.I. Angélica María Hernández Tapia', 0, 'Hospital Infantil de México Federico Gómez', 'https://res.cloudinary.com/diuxbqmn5/image/upload/v1676680009/Banner-5o-congreso-01_bligem.webp', 'https://drive.google.com/file/d/1-kP_rnGssAP3yUFdyl9Ik44_CTltaLBr/view?usp=sharing', 3, '2023-02-16', '2023-02-17', 1),
-('8º Curso para padres para el cuidado del niño con hemofilia', NULL, 500, 'Anexo A', NULL, NULL, 1, '2023-03-03', '2023-11-03', 2),
-('Atención psicológica en el paciente hospitalizado', NULL, 500, 'Aula 6', NULL, NULL, 1, '2023-04-17', '2023-04-21', 3),
-('Bioinformática, una herramienta indispensable en la investigación', '', 1500, 'Sala de videoconferencias, 6º piso del Edificio de Hemato-Oncología e Investigación', NULL, NULL, 1, '2023-04-24', '2023-04-28', 3),
-('Capacitación en micología clínica', 'M. en C.I. Jesús Reséndiz Sánchez/ I.B.T. Jorge Mauricio Díaz Barreto', 10000, 'Laboratorio', NULL, NULL, 1, '2023-03-01', '2023-04-28', 2),
-('Capacitación en micología clínica - Mayo', NULL, 10000, 'Laboratorio', NULL, NULL, 1, '2023-05-02', '2023-06-30', 4),
-('Ciclo de conferencias: grupos vulnerables en la atención social', 'Lic. Verónica Perea Rivera / Mtra. Virginia Emilia Fajardo Martínez', 0, 'Anexo A', NULL, NULL, 1, '2023-01-12', '2023-10-12', 0),
-('Construcción del conocimiento para la investigación social', NULL, 300, 'Virtual', NULL, NULL, 2, '2023-04-10', '2023-04-14', 3),
-('Curso básico de metodología de la investigación ', NULL, 0, 'Plataforma Moodle', NULL, NULL, 2, '2023-03-01', '2023-05-01', 2),
-('Curso de bioseguridad hospitalaria', 'Dra. Marcela Salazar García', 0, 'Aulas 3 y 4, 6 piso del Edificio de Hemato-Oncología eInvestigación', NULL, NULL, 2, '2023-02-01', '2023-03-01', 1),
-('Curso de preparación para el examen de grado de la especialidad de enfermería', 'E.E.I. Maricela Cruz Jiménez', 800, 'Sala de videoconferencias, 6º piso del Edificio de Hemato-Oncología e Investigación', 'https://res.cloudinary.com/diuxbqmn5/image/upload/v1676680008/examen-enfermeria-min_h8ysep.webp', 'https://drive.google.com/file/d/1QbgilSpYdjxTnkOD0ZO9hWhmhcLEbc1F/view', 1, '2023-02-08', '2023-02-10', 1),
-('Curso de reentrenamiento en operación de acelerador lineal', NULL, 0, 'Aula de Radioterapia', NULL, NULL, 1, '2023-05-22', '2023-05-26', 4),
-('Curso de reentrenamiento en seguridad radiológica', NULL, 0, 'Aula de Radioterapia', NULL, NULL, 1, '2023-05-15', '2023-05-19', 4),
-('Curso monográfico: Certificación hospitalaria, estándares\r\nnecesarios para brindar servicios de calidad', 'Mtra. Olivia Aguilar Guzmán', 800, 'Auditorio Jesús Kumate', NULL, NULL, 3, '2023-01-16', '2023-01-20', 0),
-('Curso monográfico: Terapias de reemplazo renal continuas en el paciente pediátrico', NULL, 800, 'Auditorio Jesús Kumate', NULL, NULL, 3, '2023-05-17', '2023-05-19', 4),
-('Curso sobre temas de psicología infantil y la adolescencia', NULL, 500, 'En línea', NULL, 'https://drive.google.com/file/d/1LBiFAoopQPCOnCNakM2i7q5E-trrcWDO/view?usp=sharing', 2, '2023-03-27', '2023-03-31', 2),
-('Diplomado de actualización de alta especialidad en Ortopedia Pediátrica', NULL, 1500, 'Aula 12', NULL, NULL, NULL, '2023-02-02', '2023-08-31', 1),
-('Diplomado de Encefalografía Clínica Pediátrica', NULL, 15000, 'Aula de Electroencefalografía Pediátrica', 'https://res.cloudinary.com/diuxbqmn5/image/upload/v1676680009/Diplomado-de-encefalografi-a-cli-nica-01-min_cajwhh.webp', 'https://drive.google.com/file/d/1eEfEiV_QTs60h4-mXAusuhOn-ekg5UaW/view?usp=sharing', 1, '2023-03-01', '2023-08-31', 2),
-('Diplomado en Cirugía Colorrectal Pediátrica', NULL, 10000, 'Servicio de Cirugía General', NULL, NULL, 1, '2023-03-01', '2024-02-28', 2),
-('Diplomado en medicina transfusional y banco de sangre', NULL, 13500, 'Microsoft Teams', 'https://res.cloudinary.com/diuxbqmn5/image/upload/v1676680009/diplomado-en-medicina-transfucional_uqtdc8.webp', NULL, 3, '2023-01-26', '2023-10-27', 0),
-('Diplomado: Medicina integral del adolescente y Ginecología de niñas y adolescentes', NULL, 15000, 'Aula de Medicina Integral del Adolescente', 'https://res.cloudinary.com/diuxbqmn5/image/upload/v1676680009/medicina-integral-adolescente-min_tloixo.webp', 'https://drive.google.com/file/d/107FTVYToEvp6mISq7RfCkxfwCc-X_GO1/view?usp=sharing', 1, '2023-03-01', '2024-02-29', 2),
-('Elaboración de proyectos de investigación social', 'Lic. Verónica Perea Rivera/ Lic. Martha Leticia Bureos Oaxaca', 300, 'Aula 4', NULL, NULL, 1, '2023-05-16', '2023-05-25', 4),
-('Sesiones de capacitación técnica y actualización del laboratorio clínico', 'M. en C. Israel Parra Ortega', 0, 'Aula 7', NULL, NULL, 1, '2023-01-12', '2023-12-14', 0),
-('Simposio: Procesos de calidad en la desinfeccción y estirilización de dispositivos médicos y quirúrg', NULL, 800, 'Auditorio Dr. Jesús Kumate', NULL, NULL, 3, '2023-03-08', '2023-03-10', 2),
-('Trabajo Social con familias: Retos asociados a la intervención profesional en el contexto sanitario', 'Lic. Verónica Perea Rivera / Mtra. Virginia Emilia Fajardo Martínez', 300, 'Aula 6', NULL, NULL, 1, '2023-02-13', '2023-02-17', 1),
-('Trastorno de estrés social y trastorno de estrés postraumático. Su impacto en la salud', 'Dra. Olena Pityk', 0, 'Auditorio Jesús Kumate', NULL, 'https://drive.google.com/file/d/13NDCfZqUcjotgfFomJKIQ9ITUmWYVf1B/view?usp=sharing', 1, '2023-02-02', '2023-02-02', 1),
-('VI Jornadas de medicina nuclear pediátrica', 'Dr. Rafael Delgado Espín', 0, 'Aula Magna', NULL, NULL, 1, '2023-05-26', '2023-05-26', 4),
-('VIII Curso teórico-práctico de bacteriología diagnóstica', NULL, 1500, 'Aula 8', NULL, NULL, 1, '2023-03-27', '2023-03-31', 2),
-('XVI Curso de calidad y seguridad del paciente', 'Dra. Miriam Guadalupe Herrera Segura', 600, 'Auditorio Jesús Kumate', NULL, NULL, 3, '2023-05-22', '2023-05-26', 4),
-('XXIX Curso Monográfico de Estomatología, conceptos actuales de cirugía maxilofacial, ortodoncia y es', NULL, 1000, 'Auditorio Dr. Jesús Kumate', NULL, NULL, 3, '2023-03-13', '2023-03-15', 2),
-('XXVI Curso de actualización en anestesiología pediátrica y conmemorativo del LXXX aniversario del Ho', NULL, 1800, 'Auditorio Dr. Jesús Kumate y Edificio de Enseñanza (Aula Magna y Aula 1)', NULL, NULL, 3, '2023-04-17', '2023-04-21', 3),
-('XXVIII Curso teórico-práctico de parasitología diagnóstica', 'M. en C. Israel Parra Ortega', 1500, 'Aula 1', NULL, 'https://drive.google.com/file/d/13NDCfZqUcjotgfFomJKIQ9ITUmWYVf1B/view', 1, '2023-02-13', '2023-02-17', 1);
+INSERT INTO `cursos` (`course_name`, `course_instructor`, `course_price`, `course_place`, `course_image`, `course_pdf`, `modality_id`, `course_start_date`, `course_finish_date`, `month_id`, `course_id`) VALUES
+('5to Congreso de enfermería en alta especialidad de la CCINSHAE', 'E.E.I. Angélica María Hernández Tapia', 0, 'Hospital Infantil de México Federico Gómez', 'https://res.cloudinary.com/diuxbqmn5/image/upload/v1676680009/Banner-5o-congreso-01_bligem.webp', 'https://drive.google.com/file/d/1-kP_rnGssAP3yUFdyl9Ik44_CTltaLBr/view?usp=sharing', 3, '2023-02-16', '2023-02-17', 1, 1),
+('8º Curso para padres para el cuidado del niño con hemofilia', NULL, 500, 'Anexo A', NULL, NULL, 1, '2023-03-03', '2023-11-03', 2, 2),
+('Atención psicológica en el paciente hospitalizado', NULL, 500, 'Aula 6', NULL, NULL, 1, '2023-04-17', '2023-04-21', 3, 3),
+('Bioinformática, una herramienta indispensable en la investigación', '', 1500, 'Sala de videoconferencias, 6º piso del Edificio de Hemato-Oncología e Investigación', NULL, NULL, 1, '2023-04-24', '2023-04-28', 3, 4),
+('Capacitación en micología clínica', 'M. en C.I. Jesús Reséndiz Sánchez/ I.B.T. Jorge Mauricio Díaz Barreto', 10000, 'Laboratorio', NULL, NULL, 1, '2023-03-01', '2023-04-28', 2, 5),
+('Capacitación en micología clínica - Mayo', NULL, 10000, 'Laboratorio', NULL, NULL, 1, '2023-05-02', '2023-06-30', 4, 6),
+('Ciclo de conferencias: grupos vulnerables en la atención social', 'Lic. Verónica Perea Rivera / Mtra. Virginia Emilia Fajardo Martínez', 0, 'Anexo A', NULL, NULL, 1, '2023-01-12', '2023-10-12', 0, 7),
+('Construcción del conocimiento para la investigación social', NULL, 300, 'Virtual', NULL, NULL, 2, '2023-04-10', '2023-04-14', 3, 8),
+('Curso básico de metodología de la investigación ', NULL, 0, 'Plataforma Moodle', NULL, NULL, 2, '2023-03-01', '2023-05-01', 2, 9),
+('Curso de bioseguridad hospitalaria', 'Dra. Marcela Salazar García', 0, 'Aulas 3 y 4, 6 piso del Edificio de Hemato-Oncología eInvestigación', NULL, NULL, 2, '2023-02-01', '2023-03-01', 1, 10),
+('Curso de preparación para el examen de grado de la especialidad de enfermería', 'E.E.I. Maricela Cruz Jiménez', 800, 'Sala de videoconferencias, 6º piso del Edificio de Hemato-Oncología e Investigación', 'https://res.cloudinary.com/diuxbqmn5/image/upload/v1676680008/examen-enfermeria-min_h8ysep.webp', 'https://drive.google.com/file/d/1QbgilSpYdjxTnkOD0ZO9hWhmhcLEbc1F/view', 1, '2023-02-08', '2023-02-10', 1, 11),
+('Curso de reentrenamiento en operación de acelerador lineal', NULL, 0, 'Aula de Radioterapia', NULL, NULL, 1, '2023-05-22', '2023-05-26', 4, 12),
+('Curso de reentrenamiento en seguridad radiológica', NULL, 0, 'Aula de Radioterapia', NULL, NULL, 1, '2023-05-15', '2023-05-19', 4, 13),
+('Curso monográfico: Certificación hospitalaria, estándares\r\nnecesarios para brindar servicios de calidad', 'Mtra. Olivia Aguilar Guzmán', 800, 'Auditorio Jesús Kumate', NULL, NULL, 3, '2023-01-16', '2023-01-20', 0, 14),
+('Curso monográfico: Terapias de reemplazo renal continuas en el paciente pediátrico', NULL, 800, 'Auditorio Jesús Kumate', NULL, NULL, 3, '2023-05-17', '2023-05-19', 4, 15),
+('Curso sobre temas de psicología infantil y la adolescencia', NULL, 500, 'En línea', NULL, 'https://drive.google.com/file/d/1LBiFAoopQPCOnCNakM2i7q5E-trrcWDO/view?usp=sharing', 2, '2023-03-27', '2023-03-31', 2, 16),
+('Diplomado de actualización de alta especialidad en Ortopedia Pediátrica', NULL, 1500, 'Aula 12', NULL, NULL, NULL, '2023-02-02', '2023-08-31', 1, 17),
+('Diplomado de Encefalografía Clínica Pediátrica', NULL, 15000, 'Aula de Electroencefalografía Pediátrica', 'https://res.cloudinary.com/diuxbqmn5/image/upload/v1676680009/Diplomado-de-encefalografi-a-cli-nica-01-min_cajwhh.webp', 'https://drive.google.com/file/d/1eEfEiV_QTs60h4-mXAusuhOn-ekg5UaW/view?usp=sharing', 1, '2023-03-01', '2023-08-31', 2, 18),
+('Diplomado en Cirugía Colorrectal Pediátrica', NULL, 10000, 'Servicio de Cirugía General', NULL, NULL, 1, '2023-03-01', '2024-02-28', 2, 19),
+('Diplomado en medicina transfusional y banco de sangre', NULL, 13500, 'Microsoft Teams', 'https://res.cloudinary.com/diuxbqmn5/image/upload/v1676680009/diplomado-en-medicina-transfucional_uqtdc8.webp', NULL, 3, '2023-01-26', '2023-10-27', 0, 20),
+('Diplomado: Medicina integral del adolescente y Ginecología de niñas y adolescentes', NULL, 15000, 'Aula de Medicina Integral del Adolescente', 'https://res.cloudinary.com/diuxbqmn5/image/upload/v1676680009/medicina-integral-adolescente-min_tloixo.webp', 'https://drive.google.com/file/d/107FTVYToEvp6mISq7RfCkxfwCc-X_GO1/view?usp=sharing', 1, '2023-03-01', '2024-02-29', 2, 21),
+('Elaboración de proyectos de investigación social', 'Lic. Verónica Perea Rivera/ Lic. Martha Leticia Bureos Oaxaca', 300, 'Aula 4', NULL, NULL, 1, '2023-05-16', '2023-05-25', 4, 22),
+('Sesiones de capacitación técnica y actualización del laboratorio clínico', 'M. en C. Israel Parra Ortega', 0, 'Aula 7', NULL, NULL, 1, '2023-01-12', '2023-12-14', 0, 23),
+('Simposio: Procesos de calidad en la desinfeccción y estirilización de dispositivos médicos y quirúrg', NULL, 800, 'Auditorio Dr. Jesús Kumate', NULL, NULL, 3, '2023-03-08', '2023-03-10', 2, 24),
+('Trabajo Social con familias: Retos asociados a la intervención profesional en el contexto sanitario', 'Lic. Verónica Perea Rivera / Mtra. Virginia Emilia Fajardo Martínez', 300, 'Aula 6', NULL, NULL, 1, '2023-02-13', '2023-02-17', 1, 25),
+('Trastorno de estrés social y trastorno de estrés postraumático. Su impacto en la salud', 'Dra. Olena Pityk', 0, 'Auditorio Jesús Kumate', NULL, 'https://drive.google.com/file/d/13NDCfZqUcjotgfFomJKIQ9ITUmWYVf1B/view?usp=sharing', 1, '2023-02-02', '2023-02-02', 1, 26),
+('VI Jornadas de medicina nuclear pediátrica', 'Dr. Rafael Delgado Espín', 0, 'Aula Magna', NULL, NULL, 1, '2023-05-26', '2023-05-26', 4, 27),
+('VIII Curso teórico-práctico de bacteriología diagnóstica', NULL, 1500, 'Aula 8', NULL, NULL, 1, '2023-03-27', '2023-03-31', 2, 28),
+('XVI Curso de calidad y seguridad del paciente', 'Dra. Miriam Guadalupe Herrera Segura', 600, 'Auditorio Jesús Kumate', NULL, NULL, 3, '2023-05-22', '2023-05-26', 4, 29),
+('XXIX Curso Monográfico de Estomatología, conceptos actuales de cirugía maxilofacial, ortodoncia y es', NULL, 1000, 'Auditorio Dr. Jesús Kumate', NULL, NULL, 3, '2023-03-13', '2023-03-15', 2, 30),
+('XXVI Curso de actualización en anestesiología pediátrica y conmemorativo del LXXX aniversario del Ho', NULL, 1800, 'Auditorio Dr. Jesús Kumate y Edificio de Enseñanza (Aula Magna y Aula 1)', NULL, NULL, 3, '2023-04-17', '2023-04-21', 3, 31),
+('XXVIII Curso teórico-práctico de parasitología diagnóstica', 'M. en C. Israel Parra Ortega', 1500, 'Aula 1', NULL, 'https://drive.google.com/file/d/13NDCfZqUcjotgfFomJKIQ9ITUmWYVf1B/view', 1, '2023-02-13', '2023-02-17', 1, 32);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estudiantes`
+--
+
+CREATE TABLE `estudiantes` (
+  `student_id` int(11) NOT NULL,
+  `student_name` varchar(255) NOT NULL,
+  `student_grade` varchar(255) NOT NULL,
+  `student_institution` varchar(255) NOT NULL,
+  `student_work_position` varchar(255) NOT NULL,
+  `student_phone` int(11) NOT NULL,
+  `student_email` varchar(255) DEFAULT NULL,
+  `student_age` int(11) NOT NULL,
+  `student_graduated` varchar(255) DEFAULT NULL,
+  `student_course_graduated` varchar(255) DEFAULT NULL,
+  `student_year_graduated` varchar(255) DEFAULT NULL,
+  `student_nationality` varchar(255) NOT NULL,
+  `student_specialty` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -135,9 +158,15 @@ INSERT INTO `modalidad` (`modality_id`, `modality_name`) VALUES
 -- Indices de la tabla `cursos`
 --
 ALTER TABLE `cursos`
-  ADD PRIMARY KEY (`course_name`),
+  ADD PRIMARY KEY (`course_id`),
   ADD KEY `month_id` (`month_id`),
   ADD KEY `modality_id` (`modality_id`);
+
+--
+-- Indices de la tabla `estudiantes`
+--
+ALTER TABLE `estudiantes`
+  ADD PRIMARY KEY (`student_id`);
 
 --
 -- Indices de la tabla `meses`
@@ -150,6 +179,22 @@ ALTER TABLE `meses`
 --
 ALTER TABLE `modalidad`
   ADD PRIMARY KEY (`modality_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `cursos`
+--
+ALTER TABLE `cursos`
+  MODIFY `course_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+
+--
+-- AUTO_INCREMENT de la tabla `estudiantes`
+--
+ALTER TABLE `estudiantes`
+  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Restricciones para tablas volcadas
