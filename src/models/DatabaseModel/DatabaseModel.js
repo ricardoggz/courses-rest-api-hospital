841f21a3b894af764322c232b1ff22bd3b6bae84
ricardoggz <ricardo_ggzicm@outlook.com>
2023-02-23 08:23:23 -0600

    [change]: Enviroment variables

--- src/models/DatabaseModel/DatabaseModel.js
+++ src/models/DatabaseModel/DatabaseModel.js
@@ -4,11 +4,11 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 
 export class DatabaseModel{
-    host ='localhost'
-    user ='root'
-    database ='cursos'
-    password =''
-    port =3306
+    host =process.env.HOST
+    user ='297416_cursos'
+    database =process.env.DATABASE
+    password =process.env.PASSWORD
+    port =process.env.PORT
     pool
     constructor(){
         this.pool = new mysql.createPool({
