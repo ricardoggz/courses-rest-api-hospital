f76df277e4465f8ce4f2866fe20e22d921330893
ricardoggz <ricardo_ggzicm@outlook.com>
2023-03-26 17:17:55 -0600

    [add]: courseLogin endpoint

--- src/routes/CourseRouter/CourseRouter.js
+++ src/routes/CourseRouter/CourseRouter.js
@@ -1,6 +1,7 @@
 import { Router } from 'express'
-import { getCourses } from '../../controllers/index.js'
+import { getCourses, courseLogin } from '../../controllers/index.js'
 
 export const CourseRouter = Router()
 CourseRouter
     .get('/all-courses', getCourses)
+    .post('/course-login', courseLogin)
