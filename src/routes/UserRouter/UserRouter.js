a1962d74d32aad3fda7d4cbac9983b39f8f88678
ricardoggz <ricardo_ggzicm@outlook.com>
2023-04-11 10:33:48 -0600

    [add]: new endpoint to user login

--- src/routes/UserRouter/UserRouter.js
+++ src/routes/UserRouter/UserRouter.js
@@ -1,7 +1,8 @@
 import { Router } from "express"
-import { updateUser, createUser, getUsers } from "../../controllers/index.js"
+import { updateUser, createUser, getUsers, userLogin } from "../../controllers/index.js"
 export const UserRouter = Router()
 
 UserRouter.put('/update-user/:id', updateUser)
 UserRouter.post('/create-user', createUser)
 UserRouter.get('/all-users', getUsers)
+UserRouter.post('/login-user', userLogin)
