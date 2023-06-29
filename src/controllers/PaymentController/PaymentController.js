6e7523b004077ac80b7fc346362c70bcdcc95f8d
ricardoggz <ricardo_ggzicm@outlook.com>
2023-06-29 10:21:04 -0600

    [add]: course_id in payment controller

--- src/controllers/PaymentController/PaymentController.js
+++ src/controllers/PaymentController/PaymentController.js
@@ -6,7 +6,7 @@ export const getPayments = (req, res)=>{
     db.pool.getConnection((err, conn)=>{
       if(err) throw new Error(err)
       conn.query(`
-        SELECT course_name, student_name, payment_successfull FROM pagos
+        SELECT pagos.course_id, course_name, student_name, payment_successfull FROM pagos
         INNER JOIN cursos
         ON pagos.course_id = cursos.course_id
         INNER JOIN estudiantes
