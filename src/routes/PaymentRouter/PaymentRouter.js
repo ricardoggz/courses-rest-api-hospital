725cedf61d34be08defebf1a69d4cb50c54b0b00
ricardoggz <ricardo_ggzicm@outlook.com>
2023-11-14 13:01:20 -0600

    [add]: ruta para editar pagos

--- src/routes/PaymentRouter/PaymentRouter.js
+++ src/routes/PaymentRouter/PaymentRouter.js
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import {
     getPayments,
-    addPayment
+    addPayment,
+    editPayment
 } from '../../controllers/index.js'
 
 export const PaymentRouter = Router()
 PaymentRouter
     .get('/all-payments', getPayments)
     .post('/add-payment', addPayment)
+    .put('/edit-payment', editPayment)
