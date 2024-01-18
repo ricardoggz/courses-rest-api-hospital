0c089f65852508c1283ca1fc02f1021ba5b52868
ricardoggz <ricardo_ggzicm@outlook.com>
2024-01-17 20:17:58 -0600

    se devuelve la calificacion

--- src/controllers/PaymentController/PaymentController.js
+++ src/controllers/PaymentController/PaymentController.js
@@ -27,7 +27,8 @@ export const getPayments = (req, res)=>{
         payment_reference,
         payment_invoice,
         payment_degree,
-        payment_date
+        payment_date,
+        payment_grade
         FROM pagos
         INNER JOIN cursos
         ON pagos.course_id = cursos.course_id
