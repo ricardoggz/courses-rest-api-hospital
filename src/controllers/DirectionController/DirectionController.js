91ee59dcfc679ddba01c78ac0b174e017f2524db
himfg <himfgence@gmail.com>
2025-03-25 11:59:14 -0600

    se hace el endpoint de los investigadores

--- src/controllers/DirectionController/DirectionController.js
+++ src/controllers/DirectionController/DirectionController.js
@@ -2,11 +2,30 @@ import { DatabaseModel } from "../../models/index.js"
 
 const db = new DatabaseModel()
 
-export function getDirections(req, res){
-    db.pool.getConnection((err, conn)=>{
-        if(err) return res.json(err)
-        conn.query('SELECT * FROM paginas', (err, rows)=>{
-            if(err) return res.json(err)
+export function getDirections(req, res) {
+    db.pool.getConnection((err, conn) => {
+        if (err) return res.json(err)
+        conn.query('SELECT * FROM paginas', (err, rows) => {
+            if (err) return res.json(err)
+            return res.json(rows)
+        })
+    })
+}
+
+export function getInvestigators(req, res) {
+    db.pool.getConnection((err, conn) => {
+        if (err) return res.json(err)
+        conn.query(`
+            SELECT
+            investigador_nombre,
+            investigador_foto,
+            investigador_sintesis,
+            investigadores.laboratorio_id,
+            laboratorio_nombre
+            FROM investigadores
+            INNER JOIN laboratorios
+            WHERE laboratorios.laboratorio_id = investigadores.investigador_id`, (err, rows) => {
+            if (err) return res.json(err)
             return res.json(rows)
         })
     })
--- src/routes/DirectionRouter/DirectionRouter.js
+++ src/routes/DirectionRouter/DirectionRouter.js
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import {
-    getDirections
+    getDirections, 
+    getInvestigators
 } from '../../controllers/index.js'
 
 export const DirectionRouter = Router()
 DirectionRouter
     .get('/all-directions', getDirections)
+    .get('/all-investigators', getInvestigators)
